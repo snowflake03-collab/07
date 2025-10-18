607dd45f11edaf38d9802b3c99a188e755e2b1c9
snowflake03-collab <snowflake03@sookmyung.ac.kr>
2025-10-18 17:34:15 +0900

    07-3

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int inc(int counter); 
+
 int main(void)
 { 
-    int i;
-    
-    for(i = 0; i < 5; i++)
-    {
-          int temp = 1;
-          printf("temp = %d\n", temp);
-          temp++;
-          
-    } 
-
-  system("PAUSE");	
-  return 0;
+    int i = 10;
+    printf("함수 호출 전i = %d\n", i);
+    inc(i);
+    printf("함수 호출 후i = %d\n", i);
+    system("PAUSE");
+    return 0; 
 }
+    
+    int inc(int counter)
+    {
+           counter++;
+           return counter;
+    }
+
+  
