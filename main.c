1770c860d366b1a90364df9123f485022a7f2c3b
snowflake03-collab <snowflake03@sookmyung.ac.kr>
2025-10-18 17:16:17 +0900

    07-1

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{ 
+    int flag = 1;
+    while(flag != 0)
+    {
+               int y;
+               y = 3;
+               flag = 0;  
+               y = 4;
+  
+    }
+    
+  
+  system("PAUSE");	
+  return 0;
+}
